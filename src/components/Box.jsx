93ae8abd18a013e8b40e36b00997165d9fd35f0a
nicoledl <nicolelosana@gmail.com>
2023-07-27 20:03:58 -0300

    Se agregó más frases

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { generateRandomColor } from "./generateRandomColor";
 import $ from "jquery";
+import { frasesConAutores } from "./quoteArray";
 
 const Box = () => {
-  const [number, setNumber] = useState(1);
+  const [number, setNumber] = useState(5);
 
   useEffect(() => {
     const element = document.getElementById("myButton");
@@ -12,29 +13,6 @@ const Box = () => {
     }
   }, []);
 
-  const frasesConAutores = [
-    {
-      frase:
-        "La vida es lo que sucede mientras estás ocupado haciendo otros planes.",
-      autor: "John Lennon",
-    },
-    {
-      frase: "No hay camino para la paz; la paz es el camino.",
-      autor: "Mahatma Gandhi",
-    },
-    {
-      frase: "No cuentes los días, haz que los días cuenten.",
-      autor: "Muhammad Ali",
-    },
-    {
-      frase: "El único modo de hacer un gran trabajo es amar lo que haces.",
-      autor: "Steve Jobs",
-    },
-    {
-      frase: "La creatividad es la inteligencia divirtiéndose.",
-      autor: "Albert Einstein",
-    },
-  ];
 
   const handleClick = () => {
     $("#text").animate({ opacity: 0 }, 500, function () {
@@ -50,13 +28,24 @@ const Box = () => {
   return (
     <div id="quote-box" className="container-sm">
       <p id="text">
-        <i className="fas fa-quote-left fs-3"></i> {frasesConAutores[number].frase}
+        <i className="fas fa-quote-left fs-3"></i>{" "}
+        {frasesConAutores[number].frase}
       </p>
       <p id="author">- {frasesConAutores[number].autor}</p>
-      <a id="tweet-quote" target="_blank" href="https://www.twitter.com/intent/tweet" rel="noreferrer">
+      <a
+        id="tweet-quote"
+        target="_blank"
+        href="https://www.twitter.com/intent/tweet"
+        rel="noreferrer"
+      >
         <i className="fab fa-twitter"></i>
       </a>
-      <a id="tumblr-quote" target="_blank" href="https://www.tumblr.com/login" rel="noreferrer">
+      <a
+        id="tumblr-quote"
+        target="_blank"
+        href="https://www.tumblr.com/login"
+        rel="noreferrer"
+      >
         <i className="fab fa-tumblr"></i>
       </a>
       <button id="new-quote-button" onClick={handleClick}>
---
+++ src/components/quoteArray.js
@@ -0,0 +1,70 @@
+export const frasesConAutores = [
+      {
+        frase:
+          "La vida es lo que sucede mientras estás ocupado haciendo otros planes.",
+        autor: "John Lennon",
+      },
+      {
+        frase: "No hay camino para la paz; la paz es el camino.",
+        autor: "Mahatma Gandhi",
+      },
+      {
+        frase: "No cuentes los días, haz que los días cuenten.",
+        autor: "Muhammad Ali",
+      },
+      {
+        frase: "El único modo de hacer un gran trabajo es amar lo que haces.",
+        autor: "Steve Jobs",
+      },
+      {
+        frase: "La creatividad es la inteligencia divirtiéndose.",
+        autor: "Albert Einstein",
+      },
+      {
+        frase:
+          "La felicidad no es algo que pospones para el futuro; es algo que diseñas para el presente.",
+        autor: "Jim Rohn",
+      },
+      {
+        frase:
+          "La educación es el arma más poderosa que puedes usar para cambiar el mundo.",
+        autor: "Nelson Mandela",
+      },
+      {
+        frase:
+          "El éxito no es la clave de la felicidad. La felicidad es la clave del éxito. Si amas lo que estás haciendo, serás exitoso.",
+        autor: "Albert Schweitzer",
+      },
+      {
+        frase: "La mente es como un paracaídas, solo funciona si se abre.",
+        autor: "Frank Zappa",
+      },
+      {
+        frase: "El cambio no es doloroso, la resistencia al cambio sí lo es.",
+        autor: "Dalai Lama",
+      },
+      {
+        frase:
+          "No hay pasión en jugar pequeño, en conformarse con una vida que es menor de lo que eres capaz de vivir.",
+        autor: "Nelson Mandela",
+      },
+      {
+        frase:
+          "Si no puedes hacer grandes cosas, haz pequeñas cosas de una manera grandiosa.",
+        autor: "Napoleón Hill",
+      },
+      {
+        frase:
+          "El único lugar donde el éxito viene antes del trabajo es en el diccionario.",
+        autor: "Vidal Sassoon",
+      },
+      {
+        frase: "La mejor manera de predecir el futuro es crearlo.",
+        autor: "Peter Drucker",
+      },
+      {
+        frase:
+          "El fracaso no es una opción. Todo el mundo tiene que tener éxito.",
+        autor: "Arnold Schwarzenegger",
+      },
+    ];
